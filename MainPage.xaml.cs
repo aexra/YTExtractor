8404151840ac8caf7501cf7ed7d8fd23f971c200
Никита Фомин <defdXs@yandex.ru>
2023-08-22 14:34:29 +0300

    Started refactoring

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,38 +1,13 @@
-﻿using YTExtractor;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System;
 using System.Threading.Tasks;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using static System.Net.Mime.MediaTypeNames;
-using Syroot.Windows.IO;
-using System.Data;
-using Windows.UI.Popups;
-using AngleSharp.Html.Dom;
-using YoutubeExplode.Playlists;
-using Google.Apis.YouTube.v3.Data;
-using AngleSharp.Dom;
-using System.Threading;
-
-
-// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
 namespace YTExtractor
 {
-    /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         YTAudioExtractor extractor;
