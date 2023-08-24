060429b2e72eca9fd38e7e9d8465cbe52da6f8ab
Никита Фомин <defdXs@yandex.ru>
2023-08-24 17:11:01 +0300

    Ready to create config.txt

--- ConfigManager.cs
+++ ConfigManager.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace YTExtractor
 {
-    internal class ConfigManager
+    internal class IConfigManager
     {
+        public static string ConfigPath;
+
+        public static Dictionary<string, object> LoadConf()
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+
+            return dict;
+        }
+
+        public static void SaveConf()
+        {
+
+        }
     }
 }
